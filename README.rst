317b7957fb9b712d76c5f36ba383d770c4ed8f20
Mike Lopez <dev@scidentify.info>
2014-02-21 18:54:13 -0500

    readme basic usage

--- README.rst
+++ README.rst
@@ -2,3 +2,29 @@ Readme for djbaseviews
 ------------------------------
 
 ``from APP-NAME import *``
+
+Sample usage
+=============
+
+
+The following sample illustrates how to create a basic view class and instantiate a
+form that will create a new object if instance is None. If instance is provided, it will edit the current object with the fields from the form.
+
+
+.. code-block:: python
+
+	from forms import FormClass
+
+	class SomeView(ViewBase):
+		def post(self, request):
+			instance = self.get_object(ModelClass, pk, 123)
+			form = create_form(request.POST, FormClass, instance)
+			# optionally pass additional data to set to the object after saving
+			additional_data = {'somekey': 'somevalue'}
+			# returns the form, and the object created/edited
+			form, obj = self.submit_form(form, data=additional_settings)
+			if obj:
+				return HttpResponseRedirect(reverse('some_successful_url'))
+
+
+
--- djbaseviews/views.py
+++ djbaseviews/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 # Create your views here.
+
+from django.views.generic import DetailView, ListView, TemplateView, CreateView, View
+
+class BaseView(View):
+	"""
+	Base view for View classes
+	"""
+	def get_object(self, obj, key, val):
+		"""
+		Get an object by key/value
+		"""
+		f = {key: value}
+		try:
+			return obj.objects.get(**f)
+		except obj.DoesNotExist:
+			return None
+
+	def submit_form(self, form, **kwargs):
+        """
+        Submit the form and set any additional data
+		to the object.
+		"""
+        if form.is_valid():
+            # check for save arg
+            if kwargs.get('save_arg'):
+                 obj = form.save(kwargs.get('save_arg'))
+            else:
+                obj = form.save()
+            if kwargs.get('data'):
+                for k, v in kwargs.get('data').items():
+                    setattr(obj, k, v)
+                obj.save()
+            return form, obj
+        else:
+            return form, False
+
+
+class PostViewBase(BaseView)
+	"""
+	Handles POST requests only.
+	"""
+	def get(self, request):
+		return Http404
+
+	def post(self, request):
+		pass
+
+
+def create_form(data, form_class, instance):
+    """
+    When POSTING a form, it checks for instance,
+	and if its found, it creates the form class instance
+	with that object and edits it."""
+    if instance:
+        form = form_class(instance=instance)
+        if data:
+            form = form_class(data, instance=instance)
+    else:
+        form = form_class()
+        if data:
+            form = form_class(data)
+    return form
